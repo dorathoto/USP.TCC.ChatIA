f5d1439aecffff48853f56a15bc1e3b4051d3f62
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-10-18 20:49:30 -0300

    feat cookie settings

--- USP.TCC.ChatIA.MVC/Models/ChatOptions.cs
+++ USP.TCC.ChatIA.MVC/Models/ChatOptions.cs
@@ -1,7 +1,10 @@
-﻿namespace USP.TCC.ChatIA.MVC.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace USP.TCC.ChatIA.MVC.Models;
 
 public class ChatOptions
 {
+    [Range(0.1, 1.0, ErrorMessage = "O valor deve estar entre 0.1 e 1.0")]
     public float? Temperature { get; set; } = 0.7f;
     public int? MaxTokens { get; set; } = 800;
     public float? NucleusSamplingFactor { get; set; } = 0.95f;
