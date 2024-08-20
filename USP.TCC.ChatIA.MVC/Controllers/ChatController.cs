16aad22e7652d59aae1a8e9574f30da9c9290463
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2024-08-20 16:40:53 -0300

    fix novos pacotes nuget

--- USP.TCC.ChatIA.MVC/Controllers/ChatController.cs
+++ USP.TCC.ChatIA.MVC/Controllers/ChatController.cs
@@ -21,10 +21,16 @@ public class ChatController : ControllerBase
     public async Task<IActionResult> Pergunta(ChatPergunta model)
     {
         var key = Configuration["Azure:Key"];
+        var endpoint = Configuration["Azure:EndPoint"];
+
 
         var client = new OpenAIClient(
-                new Uri("https://tccusp.openai.azure.com/"),
+                new Uri(endpoint),
                 new AzureKeyCredential(key));
+
+        AzureKeyCredential credential = new AzureKeyCredential(key);
+        AzureOpenAIClient azureClient = new(new Uri(endpoint), credential);
+        ChatClient chatClient = azureClient.GetChatClient("gpt-4");
 
         Response<ChatCompletions> responseWithoutStream = await client.GetChatCompletionsAsync(
             "_teste01",
